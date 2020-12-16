8beb6cab13e066dc8b7bf92a2ef2e5766fc09c7d
Kovács, Péter <peter.dunay.kovacs@gmail.com>
2020-12-16 23:23:25 +0100

    [Go] Add basic data structure types

--- backend/pkg/icon/icon.go
+++ backend/pkg/icon/icon.go
@@ -5,3 +5,29 @@ type IconfileDescriptor struct {
 	Format string
 	Size   string
 }
+
+// IconfileData holds data of an icon-file
+type IconfileData struct {
+	IconfileDescriptor
+	content []byte
+}
+
+// Attributes holds attributes of an icon
+type Attributes struct {
+	Name string
+}
+
+// Iconfile describes the file representation of an icon
+type Iconfile struct {
+	Attributes
+	IconfileData
+}
+
+// Descriptor describes an icon
+type Descriptor struct {
+	Attributes
+	Name       string
+	ModifiedBy string
+	Iconfiles  []IconfileDescriptor
+	Tags       []string
+}
