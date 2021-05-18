07f3ffdfd5ddc5fe79473dda48f9716dc7e1b74d
Kovács, Péter <peter.dunay.kovacs@gmail.com>
2021-05-18 11:04:14 +0200

    [DB] TestReuseExistingTag

--- backend/pkg/itests/db/add_tag_to_icon_test.go
+++ backend/pkg/itests/db/add_tag_to_icon_test.go
@@ -40,8 +40,56 @@ func (s *addTagTestSuite) TestCreateAssociateNonExistingTag() {
 	tags, err = repositories.GetExistingTags(getPool())
 	s.NoError(err)
 	s.Equal(1, len(tags))
+	s.Contains(tags, tag)
 
 	iconDesc, err = repositories.DescribeIcon(db, icon.Name)
 	s.NoError(err)
 	s.Equal(1, len(iconDesc.Tags))
+	s.Contains(iconDesc.Tags, tag)
+}
+
+func (s *addTagTestSuite) TestReuseExistingTag() {
+	var tags []string
+	var err error
+
+	var icon1 = testData[0]
+	var icon2 = testData[1]
+	const tag = "used-in-marvinjs"
+
+	err = repositories.CreateIcon(getPool(), icon1.Name, icon1.Iconfiles[0], icon1.ModifiedBy, nil)
+	s.NoError(err)
+	err = repositories.CreateIcon(getPool(), icon2.Name, icon2.Iconfiles[0], icon2.ModifiedBy, nil)
+	s.NoError(err)
+
+	err = repositories.AddTag(db, icon1.Name, tag)
+	s.NoError(err)
+
+	tags, err = repositories.GetExistingTags(getPool())
+	s.NoError(err)
+	s.Equal([]string{tag}, tags)
+
+	var iconDesc1 domain.Icon
+	iconDesc1, err = repositories.DescribeIcon(db, icon1.Name)
+	s.NoError(err)
+	s.Equal([]string{tag}, iconDesc1.Tags)
+
+	var iconDesc2 domain.Icon
+	iconDesc2, err = repositories.DescribeIcon(db, icon2.Name)
+	s.NoError(err)
+	s.Empty(iconDesc2.Tags)
+
+	err = repositories.AddTag(db, icon2.Name, tag)
+	s.NoError(err)
+
+	iconDesc1, err = repositories.DescribeIcon(db, icon1.Name)
+	s.NoError(err)
+	s.Equal([]string{tag}, iconDesc1.Tags)
+
+	iconDesc2, err = repositories.DescribeIcon(db, icon2.Name)
+	s.NoError(err)
+	s.Equal([]string{tag}, iconDesc2.Tags)
+
+	tags, err = repositories.GetExistingTags(getPool())
+	s.NoError(err)
+	s.Equal([]string{tag}, tags)
 }
