28e84c77ae7880c5e4c9b911801eb68d69199b66
Kovács, Péter <peter.dunay.kovacs@gmail.com>
2022-08-24 10:26:07 +0200

    [DEV] Fix watch.sh

--- scripts/watch.sh
+++ scripts/watch.sh
@@ -1,16 +1,32 @@
-export ICON_REPO_CONFIG_FILE=deployments/configurations/dev.json
-cmd="make build"
+export ICON_REPO_CONFIG_FILE=deployments/configurations/dev-oidc.json
+cmd="make backend"
 settle_down_secs=1
 
+backend_executable="igo-repo"
+
+project_dir="$(dirname $0)/.."
+mkdir -p ~/workspace/logs
+webpack_log=~/workspace/logs/igonrepo-webpack-build
+
+pkill webpack
+pkill "$backend_executable"
+
+cd $project_dir/web
+rm -rf $webpack_log
+webpack --watch 2>&1 | tee $webpack_log &
+cd -
+while true;
+do
+    grep 'webpack.*compiled successfully' $webpack_log && break
+    sleep 1
+done
+
 eval "$cmd" || exit 1
-count=0
 while true
 do
-  count=$((count++))
-  echo "${count}th start..."
-  ./igo-repo -l debug &
+  ./"$backend_executable" -l debug &
   sleep $settle_down_secs
-  fswatch -r -1 --event Created --event Updated --event Removed -e '.*/[.]git/.*' -e 'web/dist' -e 'web/node_modules' -e '.*/igo-repo/igo-repo$' .
-  pkill igo-repo && wait
+  fswatch -r -1 --event Created --event Updated --event Removed -e '.*/[.]git/.*' -e 'web/node_modules' -e '.*/igo-repo/igo-repo$' .
+  pkill "$backend_executable"
   eval "$cmd"
 done
