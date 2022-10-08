f8960946260e40bb7fa3809a94c3a8a17bcef874
Kovács, Péter <peter.dunay.kovacs@gmail.com>
2022-10-08 16:04:25 +0200

    [DEV] Fix keycloack starter on mac

--- deployments/dev/keycloak/build.sh
+++ deployments/dev/keycloak/build.sh
@@ -1,4 +1,4 @@
-export MY_IP=$(ip route get 1.2.3.4 | awk '{print $7}' | tr -d '\n')
+which ip && export MY_IP=$(ip route get 1.2.3.4 | awk '{print $7}' | tr -d '\n') || export MY_IP=$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}')
 
 cd deployments/dev/keycloak && \
 	docker-compose up --build -d && \
