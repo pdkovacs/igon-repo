37deefd532f900e035f51a58186c75cdb5642e2b
Péter Kovács <peter.dunay.kovacs@gmail.com>
2022-10-15 15:44:36 +0200

    [DEV] keycloak target: allow "docker compose" wo/ dash

--- deployments/dev/keycloak/build.sh
+++ deployments/dev/keycloak/build.sh
@@ -1,7 +1,10 @@
 which ip && export MY_IP=$(ip route get 1.2.3.4 | awk '{print $7}' | tr -d '\n') || export MY_IP=$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}')
 
+DOCKER_COMPOSE=docker-compose
+which docker-compose >/dev/null 2>&1 || DOCKER_COMPOSE="docker compose"
+
 cd deployments/dev/keycloak && \
-	docker-compose up --build -d && \
+	$DOCKER_COMPOSE up --build -d && \
 	./wait-for-local-keycloak.sh && \
 	./create-terraform-client.sh && \
 	cd terraform && \
